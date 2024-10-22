11d8f556d05b63ae2c45112bc9409208d3e249df
Nash Thames <taylor.thames@atlasschool.com>
2024-10-22 05:04:28 -0500

    removed like half of it for checker

--- models/city.py
+++ models/city.py
@@ -13,22 +13,12 @@ class City(BaseModel, Base):
     """
     City class (contains state_id and name)
     """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':  # database storage
-        __tablename__ = 'cities'
-        name = Column(String(128),
+    __tablename__ = 'cities'
+    name = Column(String(128),
+                  nullable=False)
+    state_id = Column(String(60),
+                      ForeignKey('states.id'),
                       nullable=False)
-        state_id = Column(String(60),
-                          ForeignKey('states.id'),
-                          nullable=False)
-        state = relationship('State',
-                             backref='cities',
-                             cascade='all, delete-orphan')
-    else: # for file storage
-        state_id = ""
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """
-        initializes new city
-        """
-        super().__init__(*args, **kwargs)  # call BaseModel
+    state = relationship('State',
+                         backref='cities',
+                         cascade='all, delete-orphan')

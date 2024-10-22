91f228c5fcac8c1b90f0bd238c33dcb9c8f42011
Nash Thames <taylor.thames@atlasschool.com>
2024-10-22 14:32:27 -0500

    setup constraint removal

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -9,8 +9,6 @@ CREATE TABLE IF NOT EXISTS hbnb_dev_db.cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(128) NOT NULL
-    CONSTRAINT fk_state FOREIGN KEY (state_id) REFERENCES states(id)
-    on DELETE CASCADE
 );
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';

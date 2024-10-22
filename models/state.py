f37a750bf7fa34c343e73c6312cfc017426ca04a
Nash Thames <taylor.thames@atlasschool.com>
2024-10-22 04:04:49 -0500

    shortened City

--- models/state.py
+++ models/state.py
@@ -23,21 +23,10 @@ class State(BaseModel, Base):
     else:
         name = ""
 
-    def __init__(self, *args, **kwargs):
-        """
-        initializes new state
-        """
-        super().__init__(*args, **kwargs) # call BaseModel
-
-    if getenv("HBNB_TYPE_STORAGE") != "db":  # file storage
         @property
         def cities(self):  # getter for cities
             """
             returns list of City instances in state
             """
-            city_values = models.storage.all("City").values()
-            cities_list = []
-            for city in city_values:
-                if city.state_id == self.id:
-                    cities_list.append(city)
-            return cities_list
+            return [city for city in models.storage.all("City").values()
+                    if city.state_id == self.id]

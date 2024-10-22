6785381ca5c3b137a5829bc57601edaf2c6cbf73
Nash Thames <taylor.thames@atlasschool.com>
2024-10-22 01:09:53 -0500

    City db, file storage

--- models/city.py
+++ models/city.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+"""
+This module contains the City class.
+"""
+import models
 from models.base_model import BaseModel
+from os import getenv
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    """
+    City class
+    """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':  # database storage
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        state = relationship('State',
+                             backref='cities')
+    else: # for file storage
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        initializes new city
+        """
+        super().__init__(*args, **kwargs)  # call BaseModel

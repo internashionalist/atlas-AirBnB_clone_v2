a33c3674c538ebb467efa58f92d73e4bd4835c54
Nash Thames <taylor.thames@atlasschool.com>
2024-10-23 14:37:15 -0500

    fixed extend_existing

--- models/amenity.py
+++ models/amenity.py
@@ -13,16 +13,16 @@ if getenv("HBNB_TYPE_STORAGE") == "db":  # database storage
     from models.base_model import Base
 
     place_amenity = Table("place_amenity", Base.metadata,
-                        Column("place_id", String(60),
-                                ForeignKey("places.id"),
-                                primary_key=True,
-                                nullable=False),
-                        Column("amenity_id", String(60),
-                                ForeignKey("amenities.id"),
-                                primary_key=True,
-                                nullable=False,
-                                extend_existing=True))
-
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False,
+                                 ),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False,
+                                 ),
+                          extend_existing=True,
+                          )
 
     class Amenity(BaseModel, Base):
         """
--- models/place.py
+++ models/place.py
@@ -12,15 +12,16 @@ if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.base_model import Base
 
     place_amenity = Table("place_amenity", Base.metadata,
-                            Column("place_id", String(60),
-                                    ForeignKey("places.id"),
-                                    primary_key=True,
-                                    nullable=False),
-                            Column("amenity_id", String(60),
-                                    ForeignKey("amenities.id"),
-                                    primary_key=True,
-                                    nullable=False),
-                            extend_existing=True)
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False
+                                 ),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False
+                                ),
+                          extend_existing=True
+                          )
     
     class Place(BaseModel, Base):
         """

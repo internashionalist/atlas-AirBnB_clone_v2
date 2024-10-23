66bcc2d4c58709694d19f1948e53ef22c01a0d30
Nash Thames <taylor.thames@atlasschool.com>
2024-10-23 03:20:09 -0500

    now handles User for both DB and file

--- models/user.py
+++ models/user.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 """
-This module contains the User class.
+This module contains the User class for both file and database storage.
 """
 from os import getenv
-import models
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
-    """
-    Defines a user by various attributes
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.base_model import Base
+    from sqlalchemy import Column, String
+    from sqlalchemy.orm import relationship
+    from models.place import Place
 
-    Attributes:
-        email (str):        email
-        password (str):     password
-        first_name (str):   first name
-        last_name (str):    last name
-        places (list):      Place instances of User
-    """
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+    class User(BaseModel, Base):
+        """
+        Defines a user by various attributes (for database storage)
+
+        Attributes:
+            email (str):        email
+            password (str):     password
+            first_name (str):   first name
+            last_name (str):    last name
+            places (list):      Place instances of User
+        """
         __tablename__ = "users"
         email = Column(String(128),
                     nullable=False)
@@ -33,14 +35,31 @@ class User(BaseModel, Base):
         places = relationship("Place",
                                 backref="user",
                                 cascade="all, delete, delete-orphan")
-    else:
+
+        def __init__(self, *args, **kwargs):
+            """
+            initializes a user
+            """
+            super().__init__(*args, **kwargs)
+
+else:
+    class User(BaseModel):
+        """
+        Defines a user by various attributes (for file storage)
+
+        Attributes:
+            email (str):        email
+            password (str):     password
+            first_name (str):   first name
+            last_name (str):    last name
+        """
         email = ""
         password = ""
         first_name = ""
         last_name = ""
 
-    def __init__(self, *args, **kwargs):
-        """
-        initializes a user
-        """
-        super().__init__(*args, **kwargs)
+        def __init__(self, *args, **kwargs):
+            """
+            initializes a user
+            """
+            super().__init__(*args, **kwargs)

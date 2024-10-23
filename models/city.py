024c49090d1b38ff43a17d5d8be79e104d7c4276
Nash Thames <taylor.thames@atlasschool.com>
2024-10-23 03:02:54 -0500

    documentation

--- models/city.py
+++ models/city.py
@@ -12,6 +12,14 @@ from sqlalchemy.orm import relationship
 class City(BaseModel, Base):
     """
     City class (contains state_id and name)
+
+    Attributes:
+        name (str):     name of city
+        state_id (str): state id
+        places (list):  Place instances in city
+
+    Methods:
+        places(self):   returns list of Place instances in city
     """
     __tablename__ = "cities"
     name = Column(String(128),

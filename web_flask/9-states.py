5b7729b00d692b48953ee767482da6691ab2daf2
Nash Thames <taylor.thames@atlasschool.com>
2024-11-03 15:06:09 -0600

    10.0

---
+++ web_flask/9-states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This module starts a Flask web application.
+"""
+
+from models import storage
+from flask import Flask, render_template
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(self):
+    """
+    Closes database session
+    """
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+def states_list():
+    """
+    Returns list of cities by state
+    """
+    states = storage.all(State).values()
+
+    for state in states:
+        state.cities.sort(key=lambda city: city.name)
+
+    return render_template("8-cities_by_states.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

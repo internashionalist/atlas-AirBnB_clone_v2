6cb488ffa8a7ef6f6e6973fae96809b0f8177a36
Nash Thames <taylor.thames@atlasschool.com>
2024-11-03 06:54:54 -0600

    6.1 - missed value=value

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -47,7 +47,7 @@ def number_template(n):
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
     value = "even" if n % 2 == 0 else "odd"
-    return render_template("6-number_odd_or_even.html", n=n)
+    return render_template("6-number_odd_or_even.html", n=n, value=value)
 
 
 if __name__ == "__main__":
